fc670a77c44e2ad9f17533edc5e0a28394380264
Kundan Kumar <kkumar.cpr@gmail.com>
2022-12-18 13:25:35 +0530

    Added new line.

--- src/main/scala/Scala_Basics/getting_started_1.scala
+++ src/main/scala/Scala_Basics/getting_started_1.scala
@@ -3,6 +3,7 @@ package Scala_Basics
 object getting_started_1 {
   def main(args: Array[String]): Unit = {
     println("Hello Scala !! I am here to  learn you.")
+    println("I am so excited to Learn Scala")
   }
 
 }
